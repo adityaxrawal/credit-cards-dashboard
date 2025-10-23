0900eefb1cc72c072498cf14a282fa094eff1476
Aditya Rawal <adityarawal@Adityas-MacBook-Pro.local>
2025-10-24 00:00:07 +0530

    Resolve the BUILD ISSUE

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   // Enable React strict mode for better development experience
   reactStrictMode: true,
   
-  // Enable SWC minification for smaller bundles
-  swcMinify: true,
-  
   // Enable compression
   compress: true,
   
